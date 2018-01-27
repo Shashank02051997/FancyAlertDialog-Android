d82b4a90709fa9ab678f0b336345ca73d18058f9
Jan Rabe <jan.rabe@exozet.com>
2018-01-27 14:06:02 +0100

    replaced visibility enum by support annotation

--- fancydialoglib/src/main/java/com/shashank/sony/fancydialoglib/Icon.java
+++
@@ -1,9 +0,0 @@
-package com.shashank.sony.fancydialoglib;
-
-/**
- * Created by Administrator on 11/20/2017.
- */
-
-public enum Icon {
-    Visible,Gone
-}
---
+++ fancydialoglib/src/main/java/com/shashank/sony/fancydialoglib/Visibility.java
@@ -0,0 +1,18 @@
+package com.shashank.sony.fancydialoglib;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import static android.view.View.GONE;
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
+/**
+ * Created by <a href="https://about.me/janrabe">Jan Rabe</a>.
+ */
+@IntDef({VISIBLE, INVISIBLE, GONE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Visibility {
+}
