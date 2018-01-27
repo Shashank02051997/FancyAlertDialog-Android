a2fb4aa1b050bbd3b5b7eef0ab4249d61bb39931
Jan Rabe <jan.rabe@exozet.com>
2018-01-27 14:11:14 +0100

    replaced activity by Weakreference context holder, replaced visibility by support annotation visibility modifier, added @ColorInt and @DrawableRes annotations to setter methods, removed show method in build method, so library user can call show when they need it instead of dialog build time, also easier re-use of dialog, also replaced builder constructor with with(Context) initializer for cleaner builder pattern

--- fancydialoglib/src/main/java/com/shashank/sony/fancydialoglib/FancyAlertDialog.java
+++ fancydialoglib/src/main/java/com/shashank/sony/fancydialoglib/FancyAlertDialog.java
@@ -1,16 +1,21 @@
 package com.shashank.sony.fancydialoglib;
 
-import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.Window;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by Shashank Singhal on 03/01/2018.
@@ -18,197 +23,208 @@ import android.widget.TextView;
 
 public class FancyAlertDialog {
 
-    private String title,message,positiveBtnText,negativeBtnText;
-    private Activity activity;
+    private String title, message, positiveBtnText, negativeBtnText;
+    @DrawableRes
     private int icon;
-    private Icon visibility;
+    @Visibility
+    private int visibility;
     private Animation animation;
-    private FancyAlertDialogListener pListener,nListener;
-    private int pBtnColor,nBtnColor,bgColor;
+    private FancyAlertDialogListener pListener, nListener;
+    @ColorInt
+    private int pBtnColor, nBtnColor, bgColor;
     private boolean cancel;
+    private Dialog dialog;
 
-
-
-    private FancyAlertDialog(Builder builder){
-        this.title=builder.title;
-        this.message=builder.message;
-        this.activity=builder.activity;
-        this.icon=builder.icon;
-        this.animation=builder.animation;
-        this.visibility=builder.visibility;
-        this.pListener=builder.pListener;
-        this.nListener=builder.nListener;
-        this.positiveBtnText=builder.positiveBtnText;
-        this.negativeBtnText=builder.negativeBtnText;
-        this.pBtnColor=builder.pBtnColor;
-        this.nBtnColor=builder.nBtnColor;
-        this.bgColor=builder.bgColor;
-        this.cancel=builder.cancel;
+    private FancyAlertDialog(Builder builder) {
+        this.title = builder.title;
+        this.message = builder.message;
+        this.icon = builder.icon;
+        this.animation = builder.animation;
+        this.visibility = builder.visibility;
+        this.pListener = builder.pListener;
+        this.nListener = builder.nListener;
+        this.positiveBtnText = builder.positiveBtnText;
+        this.negativeBtnText = builder.negativeBtnText;
+        this.pBtnColor = builder.pBtnColor;
+        this.nBtnColor = builder.nBtnColor;
+        this.bgColor = builder.bgColor;
+        this.cancel = builder.cancel;
     }
 
+    public FancyAlertDialog show() {
+        dialog.show();
+        return this;
+    }
 
-    public static class Builder{
-        private String title,message,positiveBtnText,negativeBtnText;
-        private Activity activity;
+    public FancyAlertDialog dismiss() {
+        dialog.dismiss();
+        return this;
+    }
+
+    // region builder
+
+    public static class Builder {
+
+        private String title, message, positiveBtnText, negativeBtnText;
+        @DrawableRes
         private int icon;
-        private Icon visibility;
+        @Visibility
+        private int visibility;
         private Animation animation;
-        private FancyAlertDialogListener pListener,nListener;
-        private int pBtnColor,nBtnColor,bgColor;
+        private FancyAlertDialogListener pListener, nListener;
+        @ColorInt
+        private int pBtnColor, nBtnColor, bgColor;
         private boolean cancel;
 
-        public Builder(Activity activity){
-            this.activity=activity;
+        /**
+         * Always have weak references of context in libraries to avoid memory leaks.
+         **/
+        private WeakReference<Context> context;
+        private Dialog dialog;
+
+        private Builder(Context context) {
+            // use #with
+            this.context = new WeakReference<>(context);
         }
-        public Builder setTitle(String title){
-            this.title=title;
+
+        public static Builder with(@NonNull Context context) {
+            return new Builder(context);
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
             return this;
         }
 
-        public Builder setBackgroundColor(int bgColor){
-            this.bgColor=bgColor;
+        public Builder setBackgroundColor(@ColorInt int bgColor) {
+            this.bgColor = bgColor;
             return this;
         }
 
-        public Builder setMessage(String message){
-            this.message=message;
+        public Builder setMessage(String message) {
+            this.message = message;
             return this;
         }
 
-        public Builder setPositiveBtnText(String positiveBtnText){
-            this.positiveBtnText=positiveBtnText;
+        public Builder setPositiveBtnText(String positiveBtnText) {
+            this.positiveBtnText = positiveBtnText;
             return this;
         }
 
-        public Builder setPositiveBtnBackground(int pBtnColor){
-            this.pBtnColor=pBtnColor;
+        public Builder setPositiveBtnBackground(@ColorInt int pBtnColor) {
+            this.pBtnColor = pBtnColor;
             return this;
         }
 
-        public Builder setNegativeBtnText(String negativeBtnText){
-            this.negativeBtnText=negativeBtnText;
+        public Builder setNegativeBtnText(String negativeBtnText) {
+            this.negativeBtnText = negativeBtnText;
             return this;
         }
 
-        public Builder setNegativeBtnBackground(int nBtnColor){
-            this.nBtnColor=nBtnColor;
+        public Builder setNegativeBtnBackground(@ColorInt int nBtnColor) {
+            this.nBtnColor = nBtnColor;
             return this;
         }
 
 
         //setIcon
-        public Builder setIcon(int icon, Icon visibility){
-            this.icon=icon;
-            this.visibility=visibility;
+        public Builder setIcon(@DrawableRes int icon, @Visibility int visibility) {
+            this.icon = icon;
+            this.visibility = visibility;
             return this;
         }
 
-        public Builder setAnimation(Animation animation){
-            this.animation=animation;
+        public Builder setAnimation(Animation animation) {
+            this.animation = animation;
             return this;
         }
 
         //set Positive listener
-        public Builder OnPositiveClicked(FancyAlertDialogListener pListener){
-            this.pListener=pListener;
+        public Builder onPositiveClicked(@Nullable FancyAlertDialogListener pListener) {
+            this.pListener = pListener;
             return this;
         }
 
         //set Negative listener
-        public Builder OnNegativeClicked(FancyAlertDialogListener nListener){
-            this.nListener=nListener;
+        public Builder onNegativeClicked(@Nullable FancyAlertDialogListener nListener) {
+            this.nListener = nListener;
             return this;
         }
 
-        public Builder isCancellable(boolean cancel){
-            this.cancel=cancel;
+        public Builder isCancellable(boolean cancel) {
+            this.cancel = cancel;
             return this;
         }
 
-        public FancyAlertDialog build(){
-            TextView message1,title1;
+        public FancyAlertDialog build() {
+            TextView message1, title1;
             ImageView iconImg;
-            Button nBtn,pBtn;
+            Button nBtn, pBtn;
             View view;
-            final Dialog dialog;
-            if(animation==Animation.POP)
-            dialog=new Dialog(activity,R.style.PopTheme);
-            else if(animation==Animation.SIDE)
-            dialog=new Dialog(activity,R.style.SideTheme);
-            else if(animation==Animation.SLIDE)
-            dialog=new Dialog(activity,R.style.SlideTheme);
+            if (animation == Animation.POP)
+                dialog = new Dialog(context.get(), R.style.PopTheme);
+            else if (animation == Animation.SIDE)
+                dialog = new Dialog(context.get(), R.style.SideTheme);
+            else if (animation == Animation.SLIDE)
+                dialog = new Dialog(context.get(), R.style.SlideTheme);
             else
-            dialog=new Dialog(activity);
+                dialog = new Dialog(context.get());
             dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
             dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             dialog.setCancelable(cancel);
             dialog.setContentView(R.layout.fancyalertdialog);
 
             //getting resources
-            view=(View)dialog.findViewById(R.id.background);
-            title1= (TextView) dialog.findViewById(R.id.title);
-            message1=(TextView)dialog.findViewById(R.id.message);
-            iconImg=(ImageView)dialog.findViewById(R.id.icon);
-            nBtn=(Button)dialog.findViewById(R.id.negativeBtn);
-            pBtn=(Button)dialog.findViewById(R.id.positiveBtn);
+            view = dialog.findViewById(R.id.background);
+            title1 = dialog.findViewById(R.id.title);
+            message1 = dialog.findViewById(R.id.message);
+            iconImg = dialog.findViewById(R.id.icon);
+            nBtn = dialog.findViewById(R.id.negativeBtn);
+            pBtn = dialog.findViewById(R.id.positiveBtn);
             title1.setText(title);
             message1.setText(message);
-            if(positiveBtnText!=null)
-            pBtn.setText(positiveBtnText);
-            if(pBtnColor!=0)
-            { GradientDrawable bgShape = (GradientDrawable)pBtn.getBackground();
-              bgShape.setColor(pBtnColor);
+            if (positiveBtnText != null)
+                pBtn.setText(positiveBtnText);
+            if (pBtnColor != 0) {
+                GradientDrawable bgShape = (GradientDrawable) pBtn.getBackground();
+                bgShape.setColor(pBtnColor);
             }
-            if(nBtnColor!=0)
-            { GradientDrawable bgShape = (GradientDrawable)nBtn.getBackground();
-              bgShape.setColor(nBtnColor);
+            if (nBtnColor != 0) {
+                GradientDrawable bgShape = (GradientDrawable) nBtn.getBackground();
+                bgShape.setColor(nBtnColor);
             }
-            if(negativeBtnText!=null)
-            nBtn.setText(negativeBtnText);
+            if (negativeBtnText != null)
+                nBtn.setText(negativeBtnText);
             iconImg.setImageResource(icon);
-            if(visibility==Icon.Visible)
-            iconImg.setVisibility(View.VISIBLE);
-            else
-            iconImg.setVisibility(View.GONE);
-            if(bgColor!=0)
-            view.setBackgroundColor(bgColor);
-            if(pListener!=null) {
-                pBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        pListener.OnClick();
-                        dialog.dismiss();
-                    }
+            iconImg.setVisibility(visibility);
+            if (bgColor != 0)
+                view.setBackgroundColor(bgColor);
+            if (pListener != null) {
+                pBtn.setOnClickListener(v -> {
+                    pListener.onClick(dialog);
+                    dialog.dismiss();
                 });
-            }
-            else{
-                pBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        dialog.dismiss();
-                    }
-
-                });
+            } else {
+                pBtn.setOnClickListener(v -> dialog.dismiss());
             }
 
-            if(nListener!=null){
+            if (nListener != null) {
                 nBtn.setVisibility(View.VISIBLE);
-                nBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        nListener.OnClick();
-
-                        dialog.dismiss();
-                    }
+                nBtn.setOnClickListener(v -> {
+                    nListener.onClick(dialog);
+                    dialog.dismiss();
                 });
             }
 
-
-            dialog.show();
+            // note: builder should only create an object and
+            // leave the showing/dismissing of the dialog to library user
+            // dialog.show();
 
             return new FancyAlertDialog(this);
 
         }
     }
+
+    // endregion
 
 }
